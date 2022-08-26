459eb35d1f4a5f55029dce79c35460072e7dd449
Bhawick Jain <bhawick@gmail.com>
2022-08-26 20:02:53 +0100

    implement case switch to allow search and favourites list functionality

--- searchMoviesCLI.ts
+++ searchMoviesCLI.ts
@@ -1,5 +1,5 @@
 import { question, promptCLLoop } from "readline-sync";
-import { Client } from "pg";
+import { Client, QueryResult } from "pg";
 
 async function moviesCLI() {
     //As your database is on your local machine, with default port,
@@ -7,36 +7,60 @@ async function moviesCLI() {
     //we only need to specify the (non-default) database name.
     console.log(`Greetings ${process.env.PGUSER} !`)
     const client = new Client({ database: 'omdb' });
-    await client.connect()
+    await client.connect().then(() => console.log(`successfully connected to omdb!`))
     console.log("Welcome to search-movies-cli!");
+    console.log(`
+    q       quit
+    search  search movie name
+    `)
 
-    let exit = false
-
-    while (true) {
-        console.log("\nenter a SQL query")
-        let command = question(">> ")
-        if (command === 'q') { break }
-        console.log(`searching... ${command}`)
-        console.log(`\n`)
-        try {
-        const text = "SELECT id, name, date, runtime, budget, revenue, vote_average, votes_count FROM movies WHERE LOWER(name) LIKE LOWER($1) AND kind = 'movie' ORDER BY date DESC LIMIT 10"
-        const values = [`%${command}%`]
-        const queryResults = await client.query(text, values)
-        if (queryResults.rowCount > 0) {
-        console.table(queryResults.rows)
-
+    while(true) {
+        const command = question(">> ")
+        if (command === 'q') {
+            await performDisconnect(client)
+            break
         }
-        else {
-            console.log('no results')
-        }
-        } catch(error)
-        {
-console.error(`ERROR: ${error}`)
+        switch (command) {
+            case 'q':
+                break;
+            case 'search':
+                await searchMovies(client);
+                break;
+            case 'favourites':
+                console.log('not implemented!');
+                break;
+            default:
+                console.log(`command ${command} invalid`)
+
         }
     }
+    console.log('exited')
 
+}
 
-    console.log("end")
+async function performDisconnect(client: Client) {
+    client.end().then(() => console.log('disconnected'))
+}
+
+async function searchMovies(client: Client): Promise<void> {
+    const searchTerm = question('enter search term: ')
+    const text = `SELECT name, date budget FROM movies WHERE LOWER(name) LIKE LOWER($1) AND kind = 'movie' ORDER BY date DESC LIMIT 10`
+    const value = [`%${searchTerm}%`]
+    console.log(`searching... ${searchTerm}`)
+    const queryResult = await client.query(text, value)
+    presentResults(queryResult)
+    console.log(`complete.`)
+    // console.error(`ERROR: ${err}`)
+
+    return
+}
+
+function presentResults(queryResult: QueryResult): void {
+    if (queryResult.rowCount > 0){
+        console.table(queryResult.rows)
+    } else {
+        console.log('no rows found!')
+    }
 }
 
 moviesCLI().finally(() => process.exit())
