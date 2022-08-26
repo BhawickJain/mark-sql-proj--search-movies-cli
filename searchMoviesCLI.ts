331aa2f0a2ccd195baa050e61857493d6fe2d75d
Bhawick Jain <bhawick@gmail.com>
2022-08-26 17:23:29 +0100

    simple greeting and exit option with q

--- searchMoviesCLI.ts
+++ searchMoviesCLI.ts
@@ -4,5 +4,18 @@ import { Client } from "pg";
 //As your database is on your local machine, with default port,
 //and default username and password,
 //we only need to specify the (non-default) database name.
+console.log(`Greetings ${process.env.PGUSER} !`)
 const client = new Client({ database: 'omdb' });
 console.log("Welcome to search-movies-cli!");
+
+let exit = false
+
+while (exit === false) {
+    console.log("\nenter a SQL query")
+    let command = question(">> ")
+    if (command === 'q') {
+        exit = true
+    }
+}
+
+console.log("end")
