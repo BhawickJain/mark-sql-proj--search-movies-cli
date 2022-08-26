927b3ab669b5a71d28506a0315fc561672c5f69e
Bhawick Jain <bhawick@gmail.com>
2022-08-26 17:57:00 +0100

    case sensitive querying implemented

--- searchMoviesCLI.ts
+++ searchMoviesCLI.ts
@@ -1,21 +1,42 @@
-import { question } from "readline-sync";
+import { question, promptCLLoop } from "readline-sync";
 import { Client } from "pg";
 
-//As your database is on your local machine, with default port,
-//and default username and password,
-//we only need to specify the (non-default) database name.
-console.log(`Greetings ${process.env.PGUSER} !`)
-const client = new Client({ database: 'omdb' });
-console.log("Welcome to search-movies-cli!");
+async function moviesCLI() {
+    //As your database is on your local machine, with default port,
+    //and default username and password,
+    //we only need to specify the (non-default) database name.
+    console.log(`Greetings ${process.env.PGUSER} !`)
+    const client = new Client({ database: 'omdb' });
+    await client.connect()
+    console.log("Welcome to search-movies-cli!");
 
-let exit = false
+    let exit = false
 
-while (exit === false) {
-    console.log("\nenter a SQL query")
-    let command = question(">> ")
-    if (command === 'q') {
-        exit = true
+    while (exit === false) {
+        console.log("\nenter a SQL query")
+        let command = question(">> ")
+        if (command === 'q') {
+            exit = true
+        }
+        console.log(`searching... ${command}`)
+        console.log(`\n`)
+        const text = "SELECT name, date, kind FROM movies WHERE name LIKE $1 LIMIT 10"
+        const values = [`%${command}%`]
+        const queryResults = await client.query(text, values)
+        if (queryResults.rowCount > 0) {
+        console.table(queryResults.rows)
+
+        }
+        else {
+            console.log('no results')
+        }
+        // .catch((error) => console.error(`ERROR: ${error}`))
+        // .finally(() => client.end())
+        // setTimeout(() => {}, 1000)
     }
+
+
+    console.log("end")
 }
 
-console.log("end")
+moviesCLI()
